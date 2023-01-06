d54a2d82d23763eed07ca51b11f3e1b9d8cbe7e9
ZartDrag <kartikmay25@gmail.com>
2023-01-06 23:41:37 +0530

    Completed BST

--- BinarySearchTrees/BinarySearchTrees.cpp
+++ BinarySearchTrees/BinarySearchTrees.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -35,10 +36,51 @@ Node *insertIntoBST(Node *root, int value)
     return root;
 }
 
+Node *deleteFromBST(Node *root, int value){
+    if(!root)
+        return NULL;
+
+    if(root->val == value){
+        if(!root->left && !root->right){
+            delete(root);
+            return NULL;
+        } 
+
+        if(root->left==NULL ^ root->right==NULL){
+            Node* temp = root;
+            if(root->left){
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+            delete(temp);
+            return root;
+
+        } else {
+
+            Node *temp = root->right;
+            while(temp->left)
+                temp = temp->left;
+            root->val = temp->val;
+
+            root->right = deleteFromBST(root->right, temp->val);
+        }
+
+    } else {
+        if(root->val < value){
+            root->right = deleteFromBST(root->right, value);
+        } else {
+            root->left = deleteFromBST(root->left, value);
+        }
+    }
+
+    return root;
+}
+
 Node *makeTree(Node *root)
 {
-
-    int value;
+   int value;
+   cout<<"Enter values to be inserted into the tree:";
     cin >> value;
 
     while (value != -1)
@@ -50,8 +92,40 @@ Node *makeTree(Node *root)
     return root;
 }
 
+void levelOrderTraversal(Node *root){
+
+    queue<Node*> que;
+    
+    que.push(root);
+    que.push(NULL);
+    cout<<endl;
+    while(!que.empty()){
+        Node* temp = que.front();
+        que.pop();
+
+        if(temp==NULL){
+            cout<<endl;
+
+            if(!que.empty())
+                que.push(NULL);
+        } else {
+
+            cout<<temp->val<<" ";        
+
+            if(temp->left)
+                que.push(temp->left);
+
+            if(temp->right)
+                que.push(temp->right); 
+        }
+    }
+}
+
 int main()
 {
     Node *root;
     root = makeTree(root);
+    levelOrderTraversal(root);
+    root = deleteFromBST(root, 70);
+    levelOrderTraversal(root);
 }
